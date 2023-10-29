98b6f8be6d69c926af0e7f29863c68512aff5d73
ignace <ignace@otainsight.com>
2023-10-29 14:22:02 +0100

    Revert "chore: remove example eof line"
    This reverts commit dc3bcfcd75cb4bc1b69fd0429b5754f104329bff.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { addImports, createOptions } from './steps/index.js';
+import { addEndOfLine, addImports, createOptions } from './steps/index.js';
 import type { CodemodOptions } from './types/index.js';
 
 export function runCodemod(codemodOptions: CodemodOptions): void {
   const options = createOptions(codemodOptions);
 
+  // TODO: Replace with actual steps
+  addEndOfLine(options);
   addImports(options);
 }
