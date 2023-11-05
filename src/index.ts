82825e97360952918fd82f06370323e21165410d
ignace <ignace@otainsight.com>
2023-11-05 15:13:02 +0100

    refactor: extract change-extension step

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { changeExtension } from './steps/change-extension.js';
 import { convertTests, createOptions } from './steps/index.js';
 import { removeHbsImport } from './steps/remove-import.js';
 import type { CodemodOptions } from './types/index.js';
@@ -6,5 +7,6 @@ export function runCodemod(codemodOptions: CodemodOptions): void {
   const options = createOptions(codemodOptions);
 
   convertTests(options);
+  changeExtension(options);
   removeHbsImport(options);
 }
